1624d8fbb7a6bc8fae0dc8ca233381b5e160f918
zenaldiry <zenaldiry7@gmail.com>
2026-08-31 19:55:48 -0400

    initial commit

---
+++ frontend/src/services/login.js
@@ -0,0 +1,10 @@
+/* global window, localStorage */
+import axios from 'axios'
+const baseUrl = '/api/login'
+const login = async (user) => {
+  const response = await axios.post(baseUrl, user)
+  window.localStorage.setItem('loggedBlogUser', JSON.stringify(response.data))
+  return response.data
+}
+
+export default login
